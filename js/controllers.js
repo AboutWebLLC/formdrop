c9c701d0fd56cf115adf64f73b433a35a6e3fe0c
jrowny <jrowny@gmail.com>
2012-02-22 14:33:13 -0500

    slight refactor

--- js/controllers.js
+++ js/controllers.js
@@ -1,76 +1,56 @@
-function AppCtrl($route,$cookieStore,$browser) {
-		var self = this; 
-    // define routes
-    $route.when("/add/:elementId", {template:'views/elementForm.html', controller:ElementFormCtrl});
-    $route.when("about", {template:'views/about.html', controller:AboutCtrl});
-    $route.when("help", {template:'views/help.html', controller:HelpCtrl});
-		$route.parent(this);   
-		self.drupalForm = [];
-		self.code = "";
-		self.render = function(){
-			self.code =  "<?php\n\n" + _.map(self.drupalForm, function(value){return toPHP(value,[]);}).join("\n\n") + "\n\n?>";
-			$browser.defer(function(){
-				prettyPrint();
-			},200);
-		}
-}
-
-
-function toPHP(value,parents){
-	var php = "$form['" + value.label + "'] = array(\n    '#type' => '" + value.element.id + "',\n";
-	for(var i = 0; i < value.attributes.length; i++){
-		php+="    '#" + value.attributes[i].name + "' => " + value.attributes[i].value + ",\n";
-	}
-	php+=");"
-	return php; 
-}
-
-function QuickAddCtrl(Element, $route, $location) {
-	var self = this;
-  this.elements = Element.query();
-	this.slctElement = "checkbox";
-	this.addElement = function(elementId){
-		$location.updateHash("/add/"+self.slctElement);
-	}
+function AppCtrl($route, $scope, $browser, FormService, ElementService) { 
+	//vars
+	var elements = $scope.elements = ElementService.query({elementId:'elements'}),
+			code = $scope.code = "";
+	
+	$scope.$on("render",function(){
+			if(FormService.drupalForm.length){
+				code += _.map(FormService.drupalForm, function(value){return toPHP(value,[]);}).join("\n\n");
+			}else{
+				code = "<?php \n //Empty form :( \n?>";
+			}
+			$('#code').text("<?php\n\n" + code + "\n\n?>");
+			prettyPrint();
+	});
 }
 
 function AboutCtrl(){
-	var self = this;
-}
-function HelpCtrl(){
-	var self = this;
 }
 
-ElementFormCtrl.$inject = ['Element','Attribute','$route','$location','$cookieStore','$browser'];
-function ElementFormCtrl(Element, Attribute, $route, $location, $cookieStore, $browser){ 
-	var self = this; 
-	
-	//set this form up
-	self.form = {attributes:[],
-							 element:{},
-							 label:"",
-							 children:[]}
-	
-	//Get the attributes
-	self.attributes = Attribute.query();
+function HelpCtrl(){
+}
+
+function ElementFormCtrl(ElementService, FormService, $route, $location, $browser, $scope, $rootScope){ 
+		//set this form up
+	var form = $scope.form = {attributes: [],
+							              element: {},
+							              label: "",
+							              children: []}
 	
 	//get the element details
- 	self.element = Element.get({elementId: $route.current.params.elementId}, function(element) { 
-		self.form.element = element;
-		self.form.label = element.name;
-	});
+ 	var element = $scope.element = ElementService.get({elementId: $route.current.params.elementId}, function(element) { 
+		 form.element = element;
+		 form.label = element.id;
+		 //add default attributes
+		 for(var i = 0; i < element.attributes.length; i++){
+			 if(element.attributes[i].typical){
+				 form.attributes.push({"attribute":element.attributes[i],"value":element.attributes[i].value});
+			 }
+		 }
+	 });
 	
-	self.addAttribute = function(){
-		angular.Array.add(self.form.attributes,{"name" : self.element.attributes[0].name, "value": ""});
+	$scope.addAttribute = function(){
+		 form.attributes.push({"attribute":element.attributes[0],"value":""});
 	}
 	
-	self.save = function(){
-		angular.Array.add(self.$parent.drupalForm,angular.Object.copy(self.form));
-		self.$parent.render();
-		$location.updateHash(""); 
+	$scope.remove = function(idx){
+		 form.attributes.splice(idx, 1);
 	}
 	
-	$browser.defer(function(){
-			$('.collapse').collapse();
-		}, 10);
+	$scope.save = function(){
+		FormService.drupalForm.push(angular.copy(form));
+		$rootScope.$broadcast("render");
+		$location.path("/");
+	}
 }
+ElementFormCtrl.$inject = ['ElementService','FormService','$route','$location','$browser','$scope','$rootScope'];
--- js/services.js
+++ js/services.js
@@ -1,11 +1,23 @@
-angular.service('Element', function($resource) {
- return $resource('data/:elementId.json', {}, {
-   query: {method: 'GET', params: {elementId: 'elements'}, isArray: true},
- });
+angular.module('formDrop.services', [], function($provide) {	
+		//provide an Element service TODO: make it so you can switch between 6 and 7
+	 $provide.factory('ElementService', ['$resource', function(res){
+			return res('data/7/:elementId.json', {}, {});
+	 }]);
+	 
+	 //provide an Attribute service
+	 $provide.factory('AttributeService', ['$resource', function(res){
+			return res('data/7/attributes.json', {}, {});
+	 }]);
+	 
+	 //provide a service to store/manage the form
+	 $provide.service('FormService', FormServiceProvider);
 });
 
-angular.service('Attribute', function($resource) {
- return $resource('data/attributes.json', {}, {
-   query: {method: 'GET', params: {}, isArray: true},
- });
-});
+
+
+function FormServiceProvider() {
+  this.$get = ['$rootScope',  function ($rootScope) {
+    var service = {drupalForm:[]};
+		return service;
+  }];
+}

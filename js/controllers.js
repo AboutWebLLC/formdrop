bf2eaf88ec442eb6ecf1a2c2603255379f36f20e
= <=>
2012-02-29 19:11:01 -0500

    adding tree view, edit, delete

--- js/controllers.js
+++ js/controllers.js
@@ -4,42 +4,79 @@
 function AppCtrl($route, $scope, $browser, FormService, ElementService) { 
   //vars
   var elements = $scope.elements = ElementService.query({elementId:'elements'}),
-      code = $scope.code = "";
+      code = $scope.code = "",
+			tree = $scope.tree = "";
   
   $scope.$on("render",function(){
-			code = ""
+			code = ""			
+			tree = "";
       if(FormService.drupalForm.length){
         code += _.map(FormService.drupalForm, function(value){return toPHP(value,[]);}).join("\n\n");
+				tree += "<ol class=\"sortable\">" + _.map(FormService.drupalForm, function(value){return toEditList(value);}).join("\n") + "</ol>";
       }else{
         code = "//Empty form :(";
+				tree = "<ul></ul>";
       }
 			$('#code').html("");
-      $('#code').text("<?php\n\n" + code + "\n\n?>");
+      $('#code').text("<?php\n\n" + code + "\n\n?>");			
+			$('#tree').html(tree)
+			$('#tree ol').nestedSortable({
+				disableNesting: 'no-nest',
+				forcePlaceholderSize: true,
+				handle: 'div',
+				helper:	'clone',
+				items: 'li',
+				opacity: .6,
+				placeholder: 'placeholder',
+				revert: 250,
+				tabSize: 25,
+				tolerance: 'pointer',
+				toleranceElement: '> div',
+				update: function(event, ui) {
+						alert("drag and drop doesn't currently work, but it probably will soon.");	
+				}
+			});
       prettyPrint();
   });
 }
 
 /**
+ * Remove Form Controller (Confirmation)
+ */
+function RemoveFormCtrl($scope, $route, $location, $rootScope, FormService){
+	$scope.confirmRemove = function(){
+		FormService.remove($route.current.params.elementId);
+		$rootScope.$broadcast("render");
+    $location.path("/");
+	};
+}
+
+/**
  * Element Form Controller
  */
-function ElementFormCtrl(ElementService, FormService, $route, $location, $browser, $scope, $rootScope){ 
-    //set this form up
-  var form = $scope.form = {attributes: [],
-                            element: {},
-                            label: "",
-                            children: []}
+function ElementFormCtrl(ElementService, FormService, $routeParams, $location, $browser, $scope, $rootScope){ 
+  //set this form up
+  var form = $scope.form;
+  var element = $scope.element = {},
+      mode = $scope.mode = $location.$$path.split("/")[1];
+			
+	if(mode == "edit"){
+		$scope.form = form = FormService.getById($routeParams.elementId);
+		$scope.element = element = form.element;
+	}else if(mode == "add"){
+		//When adding a new element, initialize the form
+		$scope.element = element = ElementService.get({elementId: $routeParams.elementId}, function(element) { 
+			$scope.form = form = FormService.newForm(element);
+			//add default attributes
+      for(var i = 0; i < element.attributes.length; i++){
+        if(element.attributes[i].typical){
+          form.attributes.push({"attribute":element.attributes[i],"value":element.attributes[i].value});
+        }
+      }
+		});
+	}
   
-  //get the element details
-   var element = $scope.element = ElementService.get({elementId: $route.current.params.elementId}, function(element) { 
-     form.element = element;
-     form.label = element.id;
-     //add default attributes
-     for(var i = 0; i < element.attributes.length; i++){
-       if(element.attributes[i].typical){
-         form.attributes.push({"attribute":element.attributes[i],"value":element.attributes[i].value});
-       }
-     }
-   });
+
   
   $scope.addAttribute = function(){
      form.attributes.push({"attribute":element.attributes[0],"value":""});
@@ -50,12 +87,12 @@ function ElementFormCtrl(ElementService, FormService, $route, $location, $browse
   }
   
   $scope.save = function(){
-    FormService.drupalForm.push(angular.copy(form));
+    FormService.save(form);
     $rootScope.$broadcast("render");
     $location.path("/");
   }
 }
-ElementFormCtrl.$inject = ['ElementService','FormService','$route','$location','$browser','$scope','$rootScope'];
+ElementFormCtrl.$inject = ['ElementService','FormService','$routeParams','$location','$browser','$scope','$rootScope'];
 
 //not sure if I'll even need these
 function AboutCtrl(){
--- js/services.js
+++ js/services.js
@@ -9,14 +9,75 @@ angular.module('formDrop.services', [], function($provide) {
       return res('data/7/attributes.json', {}, {});
    }]);
    
+	/**
+	  * if Angular's nextUid() was exposed to the API, we'd use that. But since we probably won't 
+		* have enough form elements for an overrun, I think a simple int counter will do
+		*/
+	var seed = 0;	
+	
+	//FormService provider
+	var formServiceProvider = function() {
+		this.$get = ['$rootScope',  function ($rootScope) {
+			var drupalForm = [],
+					service = {drupalForm : drupalForm,
+										 save : function(formElement){
+											 var toSave = findFormElementById(formElement.id, drupalForm);
+											 if(toSave){
+												 toSave = formElement;
+											 }else{
+												 drupalForm.push(formElement);
+											 }
+										 },
+										 remove : function(id){
+											 removeFormElementById(id, drupalForm);
+										 },
+										 newForm : function(element){
+											 seed++;
+											 return {id:seed, element: element, label: element.id, attributes: [], children: []};										 
+										 },
+										 getById : function(id){
+											 return findFormElementById(id, drupalForm);
+										 },};
+			return service;
+		}];
+	}
+	
+	/**
+	 * attempts to find the item in the tree
+	 * returns true if removed
+	 * returns fals if not
+	 */
+	var removeFormElementById = function(id, form){
+		for(var i = 0; i < form.length; i++){
+			if(form[i].id == id){
+				form.splice(i,1);
+				return true;
+			}
+			if(form[i].children.length) {
+				var child = removeFormElementById(id, form[i].children);
+				if(child) return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * attempts to find the item in the tree
+	 */
+	var findFormElementById = function(id, form){
+		for(var i = 0; i < form.length; i++){
+			if(form[i].id == id) return form[i];
+			if(form[i].children.length) {
+				var child = findFormElementById(id, form[i].children);
+				if(child) return child;
+			}
+		}
+		return null;
+	}
+	
    //provide a service to store/manage the form
-   $provide.service('FormService', FormServiceProvider);
+   $provide.service('FormService', formServiceProvider);
+	 
+	 
+	
 });
-
-//FormService provider
-function FormServiceProvider() {
-  this.$get = ['$rootScope',  function ($rootScope) {
-    var service = {drupalForm:[]};
-    return service;
-  }];
-}
